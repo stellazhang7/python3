379612d2199bd32ee84c0505acf38d8adb759f6b
stella <408455042@qq.com>
2019-08-18 14:34:54 +0800

    8/18/2019

---
+++ 818_OddorEven.py
@@ -0,0 +1,11 @@
+def OddOrEven():
+    x = input('please input a number:')
+    y = int(x)
+    if y % 2 == 1:
+        print(y, "is odd number")
+    else:
+        print(y, "is even number")
+
+OddOrEven()
+
+
